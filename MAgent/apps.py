49be08f7b95479be34d66be2c870fec94d758903
Xin Xiang <xxiang@mail.yu.edu>
2023-09-16 19:53:18 -0400

    Initial commit

---
+++ MAgent/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MagentConfig(AppConfig):
+    name = 'MAgent'
---
+++ MAgent/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def game_view(request):
+    return render(request, 'game.html')
