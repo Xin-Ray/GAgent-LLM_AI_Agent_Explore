63ce28ee96dc0fa1a39db75965e38207681f03b0
Xin Xiang <xxiang@mail.yu.edu>
2023-09-18 00:03:10 -0400

    second commit

--- GAgent/settings.py
+++ GAgent/settings.py
@@ -120,4 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_root")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_root")
